64039f2b9f62ca0dc265bef7d8bcd73df69d0145
Dustin Wheeler <mdwheele@gmail.com>
2021-12-30 02:06:25 -0500

    Add Tailwind CSS to the project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="flex flex-col h-screen items-center justify-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="w-32 mb-8">
+    <h1 class="font-bold text-lg">Tailwind CSS Integration</h1>
+  </div>
 </template>
 
 <script>
