86a7dde0d6d2114ac0586947842c31f6d7581cac
Dustin Wheeler <mdwheele@gmail.com>
2022-01-02 01:14:08 -0500

    Implement function for catching errors within Express request handlers

---
+++ src/api/utils/errors.js
@@ -0,0 +1,16 @@
+/**
+ * This function wraps async Express handlers, catching
+ * any exceptions and forwarding them to the Error handler
+ * for the current router.
+ * 
+ * @param {import("express").RequestHandler} handler 
+ */
+function catchErrors(handler) {
+  return async (req, res, next) => {
+    try {
+      return await handler(req, res, next)
+    } catch (e) {
+      next(e)
+    }
+  }
+}
