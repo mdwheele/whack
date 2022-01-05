7a8b87852139e73bd61af823d836c2acbb5bf58c
Dustin Wheeler <mdwheele@gmail.com>
2022-01-05 01:30:31 -0500

    Adding API endpoints for joining / leaving channels and listing current user channels

--- src/api/controllers/me.js
+++ src/api/controllers/me.js
@@ -1,7 +1,11 @@
+const ChannelMembership = require("../models/ChannelMembership")
+
 exports.show = async (req, res) => {
   res.json({})
 }
 
 exports.channels = async (req, res) => {
-  res.json({})
+  const channels = await ChannelMembership.byUser(res.locals.uid)
+
+  res.json(channels.map(channel => channel.toJSON()))
 }
