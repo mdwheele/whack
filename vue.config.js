392c4896137217720e545a8c9021c9bfa093e389
Dustin Wheeler <mdwheele@gmail.com>
2022-01-12 00:58:33 -0500

    Configure Vue dev-server with SSL certificates

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
+const fs = require('fs')
+const config = require('./src/api/config')
+
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    public: `https://${config.server.hostname}:8080`,
+    https: {
+      key: fs.readFileSync('var/ssl/whack_chat.key'),
+      cert: fs.readFileSync('var/ssl/whack_chat.crt'),
+    },
+    hot: true,
+    disableHostCheck: true
   }
 }
