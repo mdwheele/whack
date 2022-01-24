fc5cadb5e1ab7f860b72e2d9375ee174ef56c445
Dustin Wheeler <mdwheele@gmail.com>
2022-01-24 02:21:41 -0500

    Usernames must be at least one character long

--- src/components/Common/Button.vue
+++ src/components/Common/Button.vue
@@ -1,5 +1,14 @@
 <template>
-  <button :class="['flex items-center space-x-2 border rounded py-1.5 px-4 font-bold transition-all', colorClasses]">
+  <button 
+    :class="[
+      'flex items-center justify-center space-x-2 border rounded font-bold transition-all', 
+      full ? 'w-full' : '', 
+      colorClasses,
+      sizeClasses
+    ]"
+    :disabled="disabled"
+    :aria-disabled="disabled"
+  >
     <Icon v-if="icon" :name="icon" :class="['w-5 h-5 flex-shrink-0', iconClasses]" />
     <slot/>
   </button>
@@ -17,8 +26,14 @@ export default {
   props: {
     color: {
       type: String,
-      validator: val => ['green', 'white'].includes(val),
+      validator: val => ['green', 'white', 'purple'].includes(val),
       default: 'white'
+    },
+
+    size: {
+      type: String,
+      validator: val => ['base', 'lg'].includes(val),
+      default: 'base'
     },
 
     icon: {
@@ -31,23 +46,34 @@ export default {
       required: false
     },
 
+    full: Boolean,
+
     disabled: Boolean
   },
 
   setup(props) {
     const colorClasses = computed(() => {
       if (props.disabled) {
-        return 'border-gray-200 bg-gray-200 text-gray-600 '
+        return 'border-gray-200 bg-gray-200 text-gray-600 cursor-not-allowed'
       } 
 
       return {
-        green: 'border-green-800 bg-green-800 text-white',
-        white: 'border-gray-400 bg-white text-gray-800'
+        green: 'border-green-900 bg-green-900 hover:bg-green-800 text-white',
+        purple: 'border-purple-900 bg-purple-900 hover:bg-purple-800 text-white',
+        white: 'border-gray-400 bg-white text-gray-800',
       }[props.color]
     })
 
+    const sizeClasses = computed(() => {
+      return {
+        base: 'py-1.5 px-4 text-base',
+        lg: 'py-2 text-lg text-semibold'
+      }[props.size]
+    })
+
     return {
-      colorClasses
+      colorClasses,
+      sizeClasses,
     }
   }
 }
